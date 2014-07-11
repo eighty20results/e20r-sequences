1e666d3d2a450c60ca77a4da81f31d9ad53280b9
Thomas Sjolshagen <Thomas@eighty20results.com>
2014-07-11 18:34:51 -0400

    Add comments to source file

--- scheduled/crons.php
+++ scheduled/crons.php
@@ -1,5 +1,5 @@
 <?php
-/* Check for new content, email a link/dummsty to the user if new content exists. */
+/* Check for new content, email a notice to the user if new content exists. */
 if (! function_exists('pmpro_sequence_check_for_new_content')):
 
 function pmpro_sequence_check_for_new_content()
@@ -21,32 +21,40 @@ function pmpro_sequence_check_for_new_content()
         WHERE post_type = 'pmpro_sequence'
     ");
 
-	foreach($seq as $s)
+	// Loop through all defined sequences on the system
+	foreach ( $seq as $s )
 	{
-		$sequence = new PMProSequences($s->ID);
+		$sequence = new PMProSequences( $s->ID );
 
 		// Grab the settings for this sequence.
-		$seq_settings = $sequence->fetchOptions($s->ID);
+		$seq_settings = $sequence->fetchOptions( $s->ID );
 
-		// Check if sequence is configured to send member updates. Return if not.
-		if ($seq_settings->sendNotice != 1) {
+		// Check if this sequence is configured to send new content notices. Exit if not.
+		if ( $seq_settings->sendNotice != 1 ) {
 			$sequence->dbgOut('Not configured to send notices to users. Exiting!');
 			return;
 		}
 
+		// Get the posts belonging to this sequence.
 		$sequence_posts = $sequence->getPosts();
 
-		foreach($sequence_posts as $sequence_post)
+		// Iterate through all of the posts
+		foreach ( $sequence_posts as $sequence_post )
 		{
-			foreach($users as $user)
+			// Iterate through all users (TODO: Fix this as it will be slow for a large system!)
+			foreach ( $users as $user )
 			{
-				$notified = get_user_meta($user->user_id,'pmpro_seq_notified', true);
+				$notified = get_user_meta( $user->user_id,'pmpro_seq_notified', true );
 
-				if(pmpro_sequence_hasAccess($user->user_id, $sequence_post->id) && !in_array($sequence_post->id, $notified))
+				// Check whether the userID has access to this sequence post and if the post isn't previously "notified"
+				if ( pmpro_sequence_hasAccess( $user->user_id, $sequence_post->id ) && !in_array( $sequence_post->id, $notified ) )
 				{
-					$sequence->sendEmail($sequence_post->id, $user->user_id);
+					// Send the email to the user about this post
+					$sequence->sendEmail( $sequence_post->id, $user->user_id );
+
+					// Update the sequence metadata that user has been notified
 					$notified[] = $sequence_post->id;
-					update_user_meta($user->user_id, 'pmpro_seq_notified', $notified);
+					update_user_meta( $user->user_id, 'pmpro_seq_notified', $notified );
 				}
 			}
 		}
