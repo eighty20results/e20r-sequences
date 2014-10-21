cc542e94c673aac995950d1ffd6c948619140b47
Thomas Sjolshagen <Thomas@eighty20results.com>
2014-10-21 10:46:56 -0400

    Removed old CSS for min-device-pixel-ratio

--- pmpro-sequences.php
+++ pmpro-sequences.php
@@ -1654,7 +1654,7 @@ if ( ! function_exists( 'sequence_post_type_icon' )):
             only screen and (-webkit-min-device-pixel-ratio: 1.5),
             only screen and (   min--moz-device-pixel-ratio: 1.5),
             only screen and (     -o-min-device-pixel-ratio: 3/2),
-            only screen and (        min-device-pixel-ratio: 1.5),
+            /* only screen and (        min-device-pixel-ratio: 1.5), */
             only screen and (                min-resolution: 1.5dppx) {
 
                 /* Admin Menu - 16px @2x */
