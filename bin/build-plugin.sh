03fbd66ec66828e8ad34b0af4da4c2044c12269f
Thomas Sjolshagen <thomas@eighty20results.com>
2015-07-09 11:38:45 -0400

    Fix: Build plugin didn't always respect version number

--- bin/build-plugin.sh
+++ bin/build-plugin.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
+# Build script for PMPRO Sequences plugin
 #
 include=(classes email scheduled css images js languages pmpro-sequences.php README.txt)
 short_name="pmpro-sequences"
 plugin_path="${short_name}"
 version=$(egrep "^Version:" ../${short_name}.php | awk '{print $2}')
 src_path="../"
-dst_path="../build/${plugin_path}/"
+dst_path="../build/${plugin_path}"
 kit_path="../build/kits"
-kit_name="${kit_path}/${short_name}-${version}.zip"
+kit_name="${kit_path}/${short_name}-${version}"
 
-if [ -f  ${kit_name} ]
+echo "Building kit for version ${version}"
+
+mkdir -p ${kit_path}
+mkdir -p ${dst_path}
+
+if [[ -f  ${kit_name} ]]
 then
     echo "Kit is already present. Cleaning up"
     rm -rf ${dst_path}
     rm -f ${kit_name}
 fi
 
-mkdir -p ${kit_path}
-mkdir -p ${dst_path}
-
 for p in ${include[@]}; do
-
 	cp -R ${src_path}${p} ${dst_path}
 done
 
 cd ${dst_path}/..
-zip -r ${kit_name} ${short_name}-${version}
+zip -r ${kit_name}.zip ${plugin_path}
+scp ${kit_name}.zip siteground-e20r:./www/protected-content/
 rm -rf ${dst_path}
+
 
