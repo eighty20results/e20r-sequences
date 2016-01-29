7db8fffa423ceb13d9872d393880e780c2e672a4
Thomas Sjolshagen <thomas@eighty20results.com>
2016-01-29 15:04:01 -0500

    FIX: Add e20r-sequences loader file to build

--- bin/build-plugin.sh
+++ bin/build-plugin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Build script for Eighty/20 Results - Sequences plugin
 #
-include=(classes email css fonts images js languages e20r-sequences.php README.txt)
+include=(classes email css fonts images js languages e20r-sequences.php e20r-sequences-loader.php README.txt)
 exclude=(*.yml *.phar composer.* vendor)
 build=(classes/plugin-updates/vendor/*.php)
 short_name="e20r-sequences"
