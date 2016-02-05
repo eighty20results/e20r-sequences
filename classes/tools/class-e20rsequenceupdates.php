9d33c06ca774390069048d6ec24745a70196561b
Thomas Sjolshagen <thomas@eighty20results.com>
2016-02-05 09:39:40 -0500

    ENH: Initial commit for update functionality tool

---
+++ classes/tools/class-e20rsequenceupdates.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sjolshag
+ * Date: 2/5/16
+ * Time: 8:52 AM
+ */
+
+namespace E20R\Sequences\Tools;
+
+use E20R\Sequences\Tools\E20RError as E20RError;
+use E20R\Tools as E20RTools;
+
+class e20rSequenceUpdates
+{
+    private $current_version;
+    private static $_this;
+
+    public function __construct()
+    {
+        if (isset(self::$_this)) {
+            $error_message = sprintf(__("Attempted to load a second instance of this singleton class (%s)", "e20rsequence"),
+                get_class($this)
+            );
+
+            error_log($error_message);
+            wp_die( $error_message);
+
+        }
+
+        self::$_this = $this;
+
+        add_filter('get_sequence_update_class_instance', array( $this, 'get_instance'));
+        add_action('init', array($this, 'init'));
+    }
+
+    public function init() {
+
+        $plugin_status = get_plugin_data(E20R_SEQUENCE_PLUGIN_DIR, false, false);
+        $this->current_version = $plugin_status['Version'];
+
+        E20RTools\DBG::log("Running INIT for updates related to {$this->current_version}");
+
+        add_action("e20r_sequence_before_update_{$this->current_version}");
+        add_action("e20r_sequence_update_{$this->current_version}");
+        add_action("e20r_sequence_after_update_{$this->current_version}");
+    }
+
+    public function get_version()
+    {
+        return $this->current_version;
+    }
+
+    public function get_instance()
+    {
+        return self::$_this;
+    }
+
+    static public function update()
+    {
+        $su_class = apply_filters('get_sequence_update_class_instance',null);
+        $version = $su_class->get_version();
+
+        E20RTools\DBG::log("Running pre (before) update action for {$version}");
+        do_action("e20r_sequence_before_update_{$version}");
+
+        E20RTools\DBG::log("Running update action for {$version}");
+        do_action("e20r_sequence_update_{$version}");
+
+        E20RTools\DBG::log("Running clean-up (after) update action for {$version}");
+        do_action("e20r_sequence_after_update_{$version}");
+    }
+}
