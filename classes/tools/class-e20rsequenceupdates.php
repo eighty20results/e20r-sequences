aa9f4777acfe6d9ed5aae5ae303ab63df9c1786c
Thomas Sjolshagen <thomas@eighty20results.com>
2016-02-06 10:51:50 -0500

    ENH: Add license information FIX: Revert version fix

--- classes/tools/class-e20rsequenceupdates.php
+++ classes/tools/class-e20rsequenceupdates.php
@@ -1,10 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: sjolshag
- * Date: 2/5/16
- * Time: 8:52 AM
- */
+/*
+  License:
+
+	Copyright 2014-2016 Eighty / 20 Results by Wicked Strong Chicks, LLC (thomas@eighty20results.com)
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 2, as
+	published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
 
 namespace E20R\Sequences\Tools;
 
@@ -37,9 +50,11 @@ class e20rSequenceUpdates
 
     public static function init()
     {
-        $plugin_status = get_plugin_data(E20R_SEQUENCE_PLUGIN_DIR . 'e20r-sequences.php', false, false);
+        E20RTools\DBG::log("Getting plugin data for E20R Sequences");
 
-        $version = $plugin_status['Version'];
+        $plugin_status = get_plugin_data(E20R_SEQUENCE_PLUGIN_DIR . 'e20r-sequences.php', false, false);
+        $version = ( !empty($plugin_status['Version']) ? $plugin_status['Version'] : E20R_SEQUENCE_VERSION );
+
         $me = self::$_this;
         $me->set_version($version);
 
