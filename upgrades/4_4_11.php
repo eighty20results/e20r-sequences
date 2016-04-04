fb2131890395686ad66a48f5b480abc3ce0b4d7b
Thomas Sjolshagen <thomas@eighty20results.com>
2016-04-04 16:55:54 -0400

    ENH: Automatically update active membership user records w/the right startdate for the sequence(s) in the system.

---
+++ upgrades/4_4_11.php
@@ -0,0 +1,39 @@
+<?php
+use E20R\Tools as E20RTools;
+
+// Set startdate for all users and their sequences.
+function e20r_sequence_upgrade_settings_4411()
+{
+    global $wpdb;
+
+    $levels = array();
+
+    if (function_exists('pmpro_getAllLevels')) {
+
+        $levels = pmpro_getAllLevels(true, true);
+    }
+
+    $levels = apply_filters('e20r-sequences-membership-module-get-level-id-array', $levels);
+    $seq = apply_filters('get_sequence_class_instance', null);
+
+    if ($seq != null ) {
+
+        foreach( $levels as $level ) {
+
+            $sequences = $seq->sequences_for_membership_level( $level->id);
+            $u_sql = $wpdb->prepare("SELECT user_id FROM {$wpdb->pmpro_memberships_users} WHERE membership_id = %d AND status = 'active'", $level->id);
+
+            $users = $wpdb->get_col($u_sql);
+
+            foreach( $sequences as $s_id ) {
+
+                foreach ($users as $u_id ) {
+
+                    $startdate = $seq->get_user_startdate( $u_id, $level->id, $s_id );
+                    E20RTools\DBG::log("Setting startdate for user {$u_id} for sequence {$s_id} in level {$level->id}: {$startdate}");
+                }
+            }
+        }
+    }
+}
+add_action('e20r_sequence_update_4.4.10', 'e20r_sequence_upgrade_settings_4410');
