7f524215510d3fcee6ab9771b2a66ab8a916f0a7
Thomas Sjolshagen <thomas@eighty20results.com>
2016-10-17 09:34:21 -0600

    ENH: Update license file

--- classes/tools/class-dbg.php
+++ classes/tools/class-dbg.php
@@ -1,10 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: sjolshag
- * Date: 1/21/16
- * Time: 2:23 PM
- */
+/*
+License:
+
+	Copyright 2014-2016 Eighty / 20 Results by Wicked Strong Chicks, LLC (info@eighty20results.com)
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 2, as
+	published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
 
 namespace E20R\Tools;
 use E20R\Tools as E20RTools;
