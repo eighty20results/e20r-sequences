c4887cad4dc6a7291e71eeea2ed8fd942fffd9e9
Thomas Sjolshagen <thomas@eighty20results.com>
2016-12-06 13:09:33 -0700

    ENH/BUG: Support new translation architecture for WordPress

--- e20r-sequences.php
+++ e20r-sequences.php
@@ -2,12 +2,12 @@
 /*
 Plugin Name: Sequences by Eighty / 20 Results
 Plugin URI: https://eighty20results.com/e20r-sequences/
-Description: Drip feed content for your users (See website for available membership module support).
+Description: Easy to configure drip feed content plugin for your users.
 Version: 4.5.0
 Author: Thomas Sjolshagen
 Author Email: thomas@eighty20results.com
 Author URI: https://eighty20results.com/thomas-sjolshagen
-Text Domain: e20rsequence
+Text Domain: e20r-sequences
 Domain Path: /languages
 License:
 
@@ -39,7 +39,7 @@ define('E20R_SEQUENCE_PLUGIN_FILE', plugin_dir_path(__FILE__) . 'e20r-sequences.
 define('E20R_SEQUENCE_PLUGIN_URL', plugin_dir_url(__FILE__));
 
 if ( version_compare( PHP_VERSION, E20R_SEQ_REQUIRED_PHP_VERSION, '<=' ) ) {
-    add_action( 'admin_notices', create_function( '', "echo '<div class=\"error\"><p>".sprintf( __('The Sequences by Eighty / 20 Results plugin <strong>requires PHP %s or later</strong> and will not function properly without it. Please upgrade PHP on this server, or deactivate Sequences by Eighty / 20 Results.', 'e20rsequence'), E20R_SEQ_REQUIRED_PHP_VERSION ) ."</p></div>';" ) );
+    add_action( 'admin_notices', create_function( '', "echo '<div class=\"error\"><p>".sprintf( __('The Sequences by Eighty / 20 Results plugin <strong>requires PHP %s or later</strong> and will not function properly without it. Please upgrade PHP on this server, or deactivate Sequences by Eighty / 20 Results.', 'e20r-sequences'), E20R_SEQ_REQUIRED_PHP_VERSION ) ."</p></div>';" ) );
     return;
 } else {
     require_once( E20R_SEQUENCE_PLUGIN_DIR . 'e20r-sequences-loader.php');
