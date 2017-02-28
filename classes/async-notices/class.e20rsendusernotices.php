a51a159ed3239f44cdf76fa4ec8e6ee0980d0653
Thomas Sjolshagen <thomas@eighty20results.com>
2017-02-27 21:33:06 -0700

    ENHANCEMENT: Initial stub to support async processing of user notices

---
+++ classes/async-notices/class.e20rsendusernotices.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Copyright 2014-2017 Eighty / 20 Results by Wicked Strong Chicks, LLC (thomas@eighty20results.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+namespace E20R\Sequences\Tools;
+
+class e20rSendUserNotices extends E20R_Background_Process {
+
+	/**
+	 * @var string The action to perform
+	 */
+	protected $action = 'send_sequence_notice';
+
+	/**
+	 * @param mixed $item
+	 *
+	 * @return mixed
+	 */
+	public function task( $item ) {
+
+		return parent::task( $item );
+	}
+
+	/**
+	 *
+	 */
+	public function complete() {
+
+		parent::complete();
+	}
+}
