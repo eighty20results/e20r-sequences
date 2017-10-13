80b57424a02f350e3981b85a3f4882b60dfed8f4
Thomas Sjolshagen <thomas@eighty20results.com>
2017-10-13 07:23:19 -0600

    ENHANCEMENT: Transition to Singleton pattern for Sequence_Updates class ENHANCEMENT: Use Utilities submodule class in Sequence_Updates

--- classes/tools/class.sequence-updates.php
+++ classes/tools/class.sequence-updates.php
@@ -21,46 +21,50 @@
 
 namespace E20R\Sequences\Tools;
 
+use E20R\Sequences\Sequence\Controller;
+use E20R\Utilities\Utilities;
 use E20R\Tools\DBG;
 
 class Sequence_Updates
 {
     private $current_version;
-    private static $_this = null;
+    private static $instance = null;
 
     public function __construct()
     {
-        DBG::log("Loading the sequence_update class");
+    	$utils = Utilities::get_instance();
+        $utils->log("Loading the sequence_update class");
 
-        if ( null !== self::$_this ) {
+        if ( ! is_null( self::$instance ) ) {
 
-            DBG::log("Error loading the sequence update class");
+            $utils->log("Error loading the sequence update class");
             $error_message = sprintf(__("Attempted to load a second instance of this singleton class (%s)", "e20r-sequences"),
                 get_class($this)
             );
 
-            error_log($error_message);
+            $utils->log($error_message);
             wp_die( $error_message);
 
         }
 
-        self::$_this = $this;
+        self::$instance = $this;
     }
 
     public static function init()
     {
-        DBG::log("Getting plugin data for E20R Sequences");
+	    $utils = Utilities::get_instance();
+        $utils->log("Getting plugin data for E20R Sequences");
 
         $update_functions = array(
             '4.4.0', '4.4.11'
         );
 
         if (function_exists('get_plugin_data'))
-            $plugin_status = get_plugin_data(E20R_SEQUENCE_PLUGIN_DIR . 'e20r-sequences.php', false, false);
+            $plugin_status = get_plugin_data(E20R_SEQUENCE_PLUGIN_DIR . 'class.e20r-sequences.php', false, false);
 
         $version = ( !empty($plugin_status['Version']) ? $plugin_status['Version'] : E20R_SEQUENCE_VERSION );
 
-        $me = self::$_this;
+        $me = self::get_instance();
         $me->set_version($version);
 
         $is_updated = get_option('e20r-sequence-updated', array() );
@@ -69,23 +73,23 @@ class Sequence_Updates
 
         if (false === in_array( $version, $a_versions ) ) {
 
-            DBG::log("Appending {$version} to list of stuff to upgrade");
+            $utils->log("Appending {$version} to list of stuff to upgrade");
             $is_updated[$version] = false;
             $a_versions[] = $version;
         }
 
-        foreach($is_updated as $v => $status) {
+        foreach($is_updated as $upd_ver => $status) {
 
-            $upgrade_file = str_replace('.', '_', $v);
+            $upgrade_file = str_replace('.', '_', $upd_ver);
 
-            if (!empty($v) &&
+            if (!empty($upd_ver) &&
                 (file_exists(E20R_SEQUENCE_PLUGIN_DIR . "upgrades/{$upgrade_file}.php") &&
                     (false == $status)  ) ) {
 
-                DBG::log("Adding update action for {$v}");
-                add_action("e20r_sequence_before_update_{$v}", array(self::$_this, 'e20r_sequence_before_update'));
-                add_action("e20r_sequence_update_{$v}", array(self::$_this, 'e20r_sequence_update'));
-                add_action("e20r_sequence_after_update_{$v}", array(self::$_this, 'e20r_sequence_after_update'));
+                $utils->log("Adding update action for {$upd_ver}");
+                add_action("e20r_sequence_before_update_{$upd_ver}", array(self::get_instance(), 'e20r_sequence_before_update'));
+                add_action("e20r_sequence_update_{$upd_ver}", array(self::get_instance(), 'e20r_sequence_update'));
+                add_action("e20r_sequence_after_update_{$upd_ver}", array(self::get_instance(), 'e20r_sequence_after_update'));
             }
         }
 
@@ -104,18 +108,21 @@ class Sequence_Updates
 
     public static function get_instance()
     {
-        if ( null == self::$_this )
-        {
-            DBG::log("Instantiating the " . get_class(self::$_this) . " class");
-            self::$_this = new self;
+	    $utils = Utilities::get_instance();
+	    
+        if ( is_null( self::$instance ) ) {
+        	
+            $utils->log("Instantiating the " . get_class(self::$instance) . " class");
+            self::$instance = new self;
         }
 
-        return self::$_this;
+        return self::$instance;
     }
 
     static public function update()
     {
-        $su_class = apply_filters('get_sequence_update_class_instance',null);
+	    $utils = Utilities::get_instance();
+        $su_class = self::get_instance();
         $version = $su_class->get_version();
 
         if (empty($version)) {
@@ -128,34 +135,31 @@ class Sequence_Updates
 
         if (!array_key_exists( $version, $is_updated )) {
 
-            DBG::log("Appending {$version} to list of stuff to upgrade");
+            $utils->log("Appending {$version} to list of stuff to upgrade");
             $is_updated[$version] = false;
         }
 
-        foreach ($is_updated as $v => $status ) {
+        foreach ($is_updated as $update_ver => $status ) {
 
-            $upgrade_file = str_replace('.', '_', $v);
+            $upgrade_file = str_replace('.', '_', $update_ver);
 
             if (file_exists(E20R_SEQUENCE_PLUGIN_DIR . "upgrades/{$upgrade_file}.php") && (false == $status)  ) {
 
                 require_once(E20R_SEQUENCE_PLUGIN_DIR . "upgrades/{$upgrade_file}.php");
 
-                DBG::log("Running pre (before) update action for {$v}");
-                do_action("e20r_sequence_before_update_{$v}");
+                $utils->log("Running pre (before) update action for {$update_ver}");
+                do_action("e20r_sequence_before_update_{$update_ver}");
 
                 // FIXME: Will always run, every time the plugin loads (prevent this!)
-                DBG::log("Running update action for {$v}");
-                do_action("e20r_sequence_update_{$v}");
+                $utils->log("Running update action for {$update_ver}");
+                do_action("e20r_sequence_update_{$update_ver}");
 
-                DBG::log("Running clean-up (after) update action for {$v}");
-                do_action("e20r_sequence_after_update_{$v}");
+                $utils->log("Running clean-up (after) update action for {$update_ver}");
+                do_action("e20r_sequence_after_update_{$update_ver}");
 
                 if (array_key_exists($v, $is_updated)) {
                     $is_updated[$v] = true;
                 }
-            }
-            else {
-                DBG::log("No updates to do for v{$v}");
             }
         }
 
